dc8414f11f6e9f0864c858985d18f99b60739e97
Krishna Sah <sahkrishna98008@gmail.com>
2026-03-25 23:54:39 +0530

    String code

---
+++ src/main/java/com/work/strings/ReverseEachWord.java
@@ -0,0 +1,21 @@
+package com.work.strings;
+
+import java.util.Scanner;
+
+public class ReverseEachWord {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str= sc.nextLine();;
+
+        String[] words=str.split(" ");
+        for(int i=0;i<words.length;i++){
+            StringBuilder sb=new StringBuilder(words[i]);
+            sb.reverse();
+            System.out.print(sb+" ");
+
+        }
+    }
+}
+
+// Reverse word --> firstly divide the words where there is meet the space then reverse it
+// and after that print with the space
