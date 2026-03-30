0c1d5a3f05d7cb54bd570855bab423cdb192cd4f
Krishna Sah <sahkrishna98008@gmail.com>
2026-03-30 11:20:33 +0530

    some char code added

---
+++ src/main/java/com/work/strings/TwicePrintEachLetter.java
@@ -0,0 +1,15 @@
+package com.work.strings;
+
+import java.util.Scanner;
+
+public class TwicePrintEachLetter {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+
+        char[] ch=str.toCharArray();
+        for(int i=0;i<ch.length;i++){
+            System.out.print(ch[i]+""+ch[i]);
+        }
+    }
+}
